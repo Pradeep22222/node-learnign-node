0052114e2bfc9dc47d8c5bd048113541e057592d
PK <pradeepdhital124@gmail.com>
2023-01-11 01:10:32 +1100

    sendig back file

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-console.log("Hello budy");
 import express from "express";
+import path from "path";
+const __dirname = path.resolve();
 const app = express();
+app.use("/", (req, res) => {
+  res.sendFile(__dirname+"/src/form.html");
+});
 app.listen(8000, (error) => {
-    error && console.log(error);
-    console.log(`server running at http://localhost:8000`)
-})
+  error && console.log(error);
+  console.log(`server running at http://localhost:8000`);
+});
