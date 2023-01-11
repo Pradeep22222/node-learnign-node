83d025b5b95fd842af22535275623c976a3b4e52
PK <pradeepdhital124@gmail.com>
2023-01-11 23:09:37 +1100

    learning developing servers

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from "express";
 import path from "path";
 const __dirname = path.resolve();
 const app = express();
-app.use("/", (req, res) => {
-  res.sendFile(__dirname+"/src/form.html");
-});
+
 app.listen(8000, (error) => {
   error && console.log(error);
   console.log(`server running at http://localhost:8000`);
+});
+app.use("/", (req, res) => {
+  res.sendFile(__dirname + "/src/form.html");
 });
